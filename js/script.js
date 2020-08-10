eadb4dceb088fac5a7387fce8ed5db84913b9905
Nadina Milewska <milewska.nadina@gmail.com>
2020-08-10 20:48:45 +0200

    Part 1 ended

--- js/script.js
+++ js/script.js
@@ -32,14 +32,15 @@
     }
 
   /* get 'href' attribute from the clicked link */
-    
-    
+    const articleSelector = clickedElement.getAttribute('href'); 
+    console.log (articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
-
-
+    const targetArticle = document.querySelector(articleSelector);
+    
   /* add class 'active' to the correct article */
 
-
+    targetArticle.classList.add ('active');
   }
   const links = document.querySelectorAll('.titles a');
   
